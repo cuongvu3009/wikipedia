78339868f5d10d40043200e41fc090771d715d60
cuongvu3009 <cuongvu3009@gmail.com>
2022-04-27 11:28:11 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (search === '') return;
+      const endPoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
+      const response = await fetch(endPoint);
+      const data = await response.json();
+      console.log(data);
+      setResults(data.query.search);
+      setSearchInfo(data.query.searchinfo);
+      setSearch('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1 className='title'>Wikipedia</h1>
+        <form className='search-box' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            className='search-input'
+            placeholder='Search something cool here...'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button className='search-btn' type='submit'>
+            Search
+          </button>
+        </form>
+        {searchInfo.totalhits ? (
+          <p>Search results: {searchInfo.totalhits}</p>
+        ) : (
+          ''
+        )}
+        <div className='info'>
+          <div className='info-details'>
+            {results.map((result, index) => {
+              const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+              return (
+                <div className='search-result' key={index}>
+                  <h3>{result.title}</h3>
+                  <p>{result.snippet}</p>
+                  <button>
+                    <a href={url}>Read more</a>
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
